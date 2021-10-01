40d91998d12dfd6ccd65a863a0418e4e579b1e9c
Leonardo Barão <leopbarao@gmail.com>
2021-10-01 12:19:22 -0300

    refactor: simplify SignUpController.handle method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,17 +3,13 @@ import { badRequest } from '../helpers/http-helper'
 import { HttpResponse, HttpRequest } from '../protocols/http'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
-    return {
-      statusCode: 400,
-      body: new Error('Bad Request')
-    }
+    return badRequest(new Error('Bad Request'))
   }
 }
