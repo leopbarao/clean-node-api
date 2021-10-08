8e1e9783fb9c921bcd0b871454fae67d4d9f6b9b
Leonardo Barão <leopbarao@gmail.com>
2021-10-08 19:18:42 -0300

    test: ensure return content type response is xml

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -6,9 +6,18 @@ describe('Content-Type Middleware', () => {
     app.get('/test_content_type', (req, res) => {
       res.send()
     })
-
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return content type as xml', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
