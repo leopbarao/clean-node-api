a5b17442a0575f142ae3a6f517a5584a64db2967
Leonardo Barão <leopbarao@gmail.com>
2021-10-11 21:56:07 -0300

    refactor: move login protocols to a single file

---
+++ src/presentation/controllers/login/login-protocols.ts
@@ -0,0 +1,3 @@
+export { EmailValidator } from '../../protocols/email-validator'
+export { Controller, HttpRequest, HttpResponse } from '../../protocols'
+export { Authentication } from '../../../domain/usecases/authentication'
