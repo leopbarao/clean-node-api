832fefb791a7bf3c699fa2003227ce96b9c3f016
Leonardo Barão <leopbarao@gmail.com>
2021-10-12 15:59:49 -0300

    refactor: move compare fields validation to ValidationComposite

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -74,21 +74,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SignUp Controller', () => {
-  test('Returns 400 if password confirmation fails', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'invalid_password'
-      }
-    }
-
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
-  })
-
   test('Returns 400 if email is invalid', async () => {
     const { sut, emailValidatorStub } = makeSut()
     //  mockando valor default usando jest
