2a2b6f71150f6e8b066949f698ea45d9293782b2
Leonardo Barão <leopbarao@gmail.com>
2021-10-18 21:37:16 -0300

    refactor: create logcontrollerdecorator factory

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,13 +1,11 @@
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
 import { makeSignUpValidation } from './signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
   const signUpController = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  return makeLogControllerDecorator(signUpController)
 }
