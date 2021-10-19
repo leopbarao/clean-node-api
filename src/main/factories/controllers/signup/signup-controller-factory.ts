c0782768585e53fd1dec1e3798032fea3d608417
Leonardo Barão <leopbarao@gmail.com>
2021-10-18 21:21:12 -0300

    refactor: create dbauthentication and dbaddaccount factories in main layer

---
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,13 @@
+import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
+import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
+import { Controller } from '../../../../presentation/protocols'
+import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeSignUpValidation } from './signup-validation-factory'
+
+export const makeSignUpController = (): Controller => {
+  const signUpController = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
+}
