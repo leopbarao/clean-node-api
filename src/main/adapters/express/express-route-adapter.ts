262b0ef1bd29d8154a744deda086a42399cdf370
Leonardo Barão <leopbarao@gmail.com>
2021-10-19 21:39:45 -0300

    feat: enable express route /surveys and make controller factory

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -8,7 +8,7 @@ export const adaptRoute = (controller: Controller): RequestHandler => {
     }
 
     const httpResponse = await controller.handle(httpRequest)
-    if (httpResponse.statusCode === 200) {
+    if (httpResponse.statusCode === 200 || httpResponse.statusCode === 204) {
       resp.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
       resp.status(httpResponse.statusCode).json({
