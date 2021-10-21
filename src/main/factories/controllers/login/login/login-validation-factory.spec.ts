cb0319d2c29030a7223ec341ec18b99bbe6286a4
Leonardo Barão <leopbarao@gmail.com>
2021-10-21 12:32:47 -0300

    refactor: organize main layer folders

--- src/main/factories/controllers/login/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login/login-validation-factory.spec.ts
@@ -1,9 +1,9 @@
 import { makeLoginValidation } from './login-validation-factory'
-import { Validation } from '../../../../presentation/protocols'
-import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
-import { EmailValidator } from '../../../../validation/protocols/email-validator'
+import { Validation } from '../../../../../presentation/protocols'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../../validation/validators'
+import { EmailValidator } from '../../../../../validation/protocols/email-validator'
 
-jest.mock('../../../../validation/validators/validation-composite')
+jest.mock('../../../../../validation/validators/validation-composite')
 
 //  factory
 const makeEmailValidator = (): EmailValidator => {
