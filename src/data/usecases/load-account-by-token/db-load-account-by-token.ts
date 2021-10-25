4e7bd3b9967e9ba4a2e2147f8e64dd11406910db
Leonardo Barão <leopbarao@gmail.com>
2021-10-25 15:03:11 -0300

    refactor: move protocols DataLayer use cases to a single file

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,8 +1,9 @@
-import { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
-import { Decrypter } from '@/data/protocols/criptography/decrypter'
-import { LoadAccountByTokenRepository } from '@/data/protocols/db/account/load-account-by-token-repository'
-import { AccountModel } from '@/data/usecases/add-account/db-add-account-protocols'
-
+import {
+  LoadAccountByToken,
+  LoadAccountByTokenRepository,
+  Decrypter,
+  AccountModel
+} from './db-load-account-by-token-protocols'
 export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (
     private readonly decrypter: Decrypter,
