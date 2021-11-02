98b4c8fe954c28857368d25d071e9bfb164e65b8
Leonardo Barão <leopbarao@gmail.com>
2021-11-02 12:37:59 -0300

    feat: add swagger api-docs

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API do curso de TDD com Node.js - Sistema de enquetes',
+    version: '1.0.0'
+  }
+}
